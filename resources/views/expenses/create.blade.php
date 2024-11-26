a61396af1a663c22ce5712d0f1b357d7d17e38d3
omartins-zs <gabrielmatheus209@gmail.com>
2024-11-26 14:17:32 -0300

    Add Formulário de criação de despesa

--- resources/views/expenses/create.blade.php
+++ resources/views/expenses/create.blade.php
@@ -1,3 +1,27 @@
-<div>
-    <!-- Nothing in life is to be feared, it is only to be understood. Now is the time to understand more, so that we may fear less. - Marie Curie -->
-</div>
+<form action="{{ route('expenses.store') }}" method="POST">
+    @csrf
+    <div>
+        <label for="description">Descrição:</label>
+        <input type="text" name="description" id="description" required>
+    </div>
+
+    <div>
+        <label for="amount">Valor:</label>
+        <input type="number" name="amount" id="amount" required step="0.01">
+    </div>
+
+    <div>
+        <label for="type">Tipo:</label>
+        <select name="type" id="type">
+            <option value="expense">Despesa</option>
+            <option value="income">Receita</option>
+        </select>
+    </div>
+
+    <div>
+        <label for="category">Categoria:</label>
+        <input type="text" name="category" id="category" required>
+    </div>
+
+    <button type="submit">Salvar</button>
+</form>
