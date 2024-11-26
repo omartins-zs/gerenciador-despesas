fd28424ad3d4411e2d2cfa80a392b39f3024dce6
omartins-zs <gabrielmatheus209@gmail.com>
2024-11-26 14:04:49 -0300

    Ajustando a Estrutura da Tabela de Despesas

--- database/migrations/2024_11_26_170221_create_expenses_table.php
+++ database/migrations/2024_11_26_170221_create_expenses_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('expenses', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('description');
+            $table->decimal('amount', 10, 2);
+            $table->enum('type', ['expense', 'income']);
+            $table->string('category');
             $table->timestamps();
         });
     }
