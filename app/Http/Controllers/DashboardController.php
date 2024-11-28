26f2f983e9c1c3e696f160af210d6d4a3a02adfe
omartins-zs <gabrielmatheus209@gmail.com>
2024-11-28 11:36:27 -0300

    Criando Function Dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,8 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
-    //
+    public function dashboard()
+    {
+        $transactions = Auth::user()->transactions()->latest()->limit(5)->get();
+
+        $totalIncome = $transactions->where('type', 'income')->sum('amount');
+        $totalExpense = $transactions->where('type', 'expense')->sum('amount');
+        $netBalance = $totalIncome - $totalExpense;
+
+        return view('dashboard', compact('totalIncome', 'totalExpense', 'netBalance', 'transactions'));
+    }
 }
